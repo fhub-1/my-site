7b836d2251b0281cedf1423d4c2c69609f484208
fhub-1 <flutterhub1.21.0@gmail.com>
2022-01-24 11:55:17 +0300

    testing

--- .history/src/App_20220122121559.js
+++ .history/src/App_20220122121559.js
@@ -9,7 +9,7 @@ import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
-    <main className="text-gray-400 bg-indigo-900 body-font">
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
